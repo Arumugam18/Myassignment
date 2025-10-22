3879f650d6da705d170d3982eb7f5a2a74d92b24
Arumugam18 <103849818+Arumugam18@users.noreply.github.com>
2025-10-22 23:24:45 +0530

    Commit Week 2 day1

---
+++ Oddnumber.js
@@ -0,0 +1,9 @@
+//odd number
+let number=30
+for (let i=0;i<=number;i++){
+    
+    if (i%2!=0){
+        console.log("Odd Number is: "+i)
+    }
+
+} 
---
+++ Switch.js
@@ -0,0 +1,22 @@
+//switch case
+let day=4
+switch(day) {
+  case "monday":
+    console.log("Print day as monday")
+    break;
+    case "tuesday":
+    console.log("Print day as tuesday")
+    break;
+    case "wednesday":
+    console.log("Print day as wednesday")
+    break;
+    case "thursday":
+    console.log("Print day as thursday")
+    break;
+    case "friday":
+    console.log("Print day as friday")
+    break;
+    default:
+    console.log("Print day as Not valid")
+    break;
+}
